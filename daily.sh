063180dab3217ef6eb63f26350489b2ff90d71d0
tcpick <towangtaylor@gmail.com>
2021-04-22 17:46:58 +0800

    update script to set ./cur/ as working dir

--- daily.sh
+++ daily.sh
@@ -3,8 +3,8 @@ if cd /home/pick/mine/ledger; then
 	d=`date +%Y%m%d`
 	./check_day.py $d
 	./check_day.py sum
-	git add outcome_$d.json
-	git add total.json
+	git add ./cur/outcome_$d.json
+	git add ./cur/total.json
 	git commit -m "date $d"
 	git push origin master
 fi
