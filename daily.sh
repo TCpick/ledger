cd4b8192235c14420aa5c8c0164bc7429c0aef10
tcpick <towangtaylor@gmail.com>
2021-04-27 08:14:58 +0800

    0427 morning update

--- daily.sh
+++ daily.sh
@@ -3,9 +3,10 @@ if cd /home/pick/mine/ledger; then
 	d=`date +%Y%m%d`
 	./check_day.py $d
 	./check_day.py sum
-	git add ./cur/outcome_${d}AM.json
-	git add ./cur/outcome_${d}PM.json
-	git add ./cur/total.json
+	#git add --ignore-errors ./cur/outcome_${d}AM.json 
+	#git add --ignore-errors ./cur/outcome_${d}PM.json 
+	#git add ./cur/total.json
+	git add --all
 	git commit -m "date $d"
 	git push origin master
 fi
